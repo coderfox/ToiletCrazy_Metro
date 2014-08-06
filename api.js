b9b2c7f756410d6b993d5f26bbfd65303beb0b7f
小傅Fox <xfox@cotr.me>
2014-08-06 11:00:57 +0800

    work with new api

--- api.js
+++ api.js
@@ -1,29 +1,31 @@
-var config={
-	"site_title":"TC",
-	"api_uri":"back/api.php"
+var config = {
+	"site_title" : "TC",
+	"api_uri" : "back/api.php"
 };
-var api_call = function  (method,params,callback) {
-	var func=null;
-	if(method==="GET"){
-		func=$.get;
-	}else if(method==="POST"){
-		func=$.post;
+var api_call = function(method, c, params, callback) {
+	var func = null;
+	if (method === "GET") {
+		func = $.get;
+	} else if (method === "POST") {
+		func = $.post;
 	}
-	func(config["api_uri"],params,callback,"json");
+	p = params;
+	p.c = c;
+	func(config["api_uri"], p, callback, "json");
 };
 $.extend({
-  getUrlVars: function(){
-    var vars = [], hash;
-    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-    for(var i = 0; i < hashes.length; i++)
-    {
-      hash = hashes[i].split('=');
-      vars.push(hash[0]);
-      vars[hash[0]] = hash[1];
-    }
-    return vars;
-  },
-  getUrlVar: function(name){
-    return $.getUrlVars()[name];
-  }
+	getUrlVars : function() {
+		var vars = [], hash;
+		var hashes = window.location.href.slice(
+				window.location.href.indexOf('?') + 1).split('&');
+		for ( var i = 0; i < hashes.length; i++) {
+			hash = hashes[i].split('=');
+			vars.push(hash[0]);
+			vars[hash[0]] = hash[1];
+		}
+		return vars;
+	},
+	getUrlVar : function(name) {
+		return $.getUrlVars()[name];
+	}
 });
